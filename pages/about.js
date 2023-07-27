57346d4c97357214f2c6b68c2d0b1ac63916dc36
ikkiwar <ikkiofbodom@gmail.com>
2023-07-27 01:11:38 -0600

    typescript configurated

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbars/Navbar";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <div>
+      <Navbar />
+      <p>about</p>{" "}
+    </div>
+  );
 };
 
 export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbars/Navbar";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <Navbar />
+      <h1>Home</h1>
+    </div>
+  );
 };
 
 export default index;
